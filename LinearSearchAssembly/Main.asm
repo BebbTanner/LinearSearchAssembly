2331e4196a288b129cee662ba795dff4a795f470
Tanner Bebb <17tbebb@gmail.com>
2024-02-15 10:07:22 -0800

    Initial setup.

---
+++ LinearSearchAssembly/Main.asm
@@ -0,0 +1,16 @@
+;Tanner Bebb
+;Linear Search Practice
+
+.386
+include Irvine32.inc
+.model flat, stdcall
+.stack 4096
+ExitProcess proto dwExitCode:dword
+
+.data
+
+.code
+main PROC
+
+main endp
+end main
