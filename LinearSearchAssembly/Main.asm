781e56f5e69318bad48242103a31cef77bccfd75
Tanner Bebb <17tbebb@gmail.com>
2024-02-21 08:41:28 -0800

    In the main proc I finished setting up what is needed to search for the listed values. Declared and defined the i variable. Started defining the LinearSearch function.

--- LinearSearchAssembly/Main.asm
+++ LinearSearchAssembly/Main.asm
@@ -10,6 +10,7 @@ ExitProcess proto dwExitCode:dword
 .data
 
 myArray SWORD 2, 9, 3, 4, 8, 1, 5
+i SDWORD 0
 
 .code
 ;EDX - OFFSET 32 bit int array
@@ -18,20 +19,40 @@ myArray SWORD 2, 9, 3, 4, 8, 1, 5
 ;EAX - return value
 
 
-LinearSeach PROC			;This is the linearsearch function
-	mov eax,0				;Storing 0 into the eax register for testing purposes
-	ret						;Return the values stored in eax?
+LinearSeach PROC				;This is the linearsearch function
+	mov i,0						;Storing 0 into the i variable
+	mov eax, edx				;eax is the pointer for the myArray
 
 LinearSearch ENDP
 
+again:
+	cmp	i, ecx					;Compares the value of i to what is stored in the ecx register.
+	jge cantfind				;If the value is greater than or equal to it jumps to the cantfind tag.
+
+
+	inc i						;Increments the value stored in i.
+	jmp again					;Jumps back to the again tag, Keeps running until the conditions are met.
+
+cantfind:
+	mov eax, -1					;Moving the value of -1 into the eax registers. This basically tells me that the value could not be found
+
+done:
+	ret							;Just a return statement, unsure as to what I am doing with this.
 
 main PROC
-	mov edx, myArray
-	mov ecx, 7
-	call LinearSearch
-	call WriteInt
-	call Crlf
+	mov edx, OFFSET myArray		;Move the OFFSET myArray into the edx register.
+	mov ecx, 7					;Move the "size" of the array into ecx.
+	mov ebx, 1					;Move the value I am searching for into ebx.
+	call LinearSearch			;This calls the LinearSeach function.
+	call WriteInt				;This calls the WriteInt function included in the irvine library.
+	call Crlf					;This calls the Crlf function which will input a blank line.
 
+	mov edx, OFFSET myArray		;Move the OFFSET myArray into the edx register.
+	mov ecx, 7					;Move the "size" of the array into ecx.
+	mov ebx, 11					;Move the value I am searching for into ebx.
+	call LinearSearch			;This calls the LinearSeach function.
+	call WriteInt				;This calls the WriteInt function included in the irvine library.
+	call Crlf					;This calls the Crlf function which will input a blank line.
 
 	call WaitMsg
 	exit
