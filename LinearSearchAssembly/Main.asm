c9d3186262b2a08d77ab3aee10a06152daa09c01
Tanner Bebb <17tbebb@gmail.com>
2024-02-21 08:09:26 -0800

    Declared and defined myArray and gave it 7 values. Move the array to the edx register. moved the value of the size of the array to the ecx register. Setup to create the LinearSearch function.

--- LinearSearchAssembly/Main.asm
+++ LinearSearchAssembly/Main.asm
@@ -9,7 +9,7 @@ ExitProcess proto dwExitCode:dword
 
 .data
 
-
+myArray SWORD 2, 9, 3, 4, 8, 1, 5
 
 .code
 ;EDX - OFFSET 32 bit int array
@@ -17,13 +17,22 @@ ExitProcess proto dwExitCode:dword
 ;EBX - 32 bit int value
 ;EAX - return value
 
+
+LinearSeach PROC			;This is the linearsearch function
+
+
+LinearSearch endp
+
+
 main PROC
+	mov edx, myArray
+	mov ecx, 7
+	LinearSearch
 
 
 
-
-call WaitMsg
-exit
+	call WaitMsg
+	exit
 
 main endp
 end main
