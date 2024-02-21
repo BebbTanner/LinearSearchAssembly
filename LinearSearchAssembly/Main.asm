510684f199c6aa5b11e51acf02cd5e4bd050d6e3
Tanner Bebb <17tbebb@gmail.com>
2024-02-21 08:03:53 -0800

    Set up the objectives for this programing excersize.

--- LinearSearchAssembly/Main.asm
+++ LinearSearchAssembly/Main.asm
@@ -9,8 +9,21 @@ ExitProcess proto dwExitCode:dword
 
 .data
 
+
+
 .code
+;EDX - OFFSET 32 bit int array
+;ECX - 32 bit int size
+;EBX - 32 bit int value
+;EAX - return value
+
 main PROC
+
+
+
+
+call WaitMsg
+exit
 
 main endp
 end main
